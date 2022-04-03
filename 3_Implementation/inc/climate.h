58457f7fefe37ef7e865e1271f18b8115c6191e1
NARENDAREN <101088188+NARENDAREN@users.noreply.github.com>
2022-04-03 17:40:07 +0530

    Add files via upload

---
+++ 3_Implementation/inc/climate.h
@@ -0,0 +1,25 @@
+#ifndef __CLIMATE_H__
+#define __CLIMATE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <climate.h>
+
+
+int mode(int m);
+
+
+int sensor1(int s1);
+
+
+int sensor2(int s2);
+
+
+int temp(int c);
+
+
+int normal(int ch);
+
+
+
+#endif
